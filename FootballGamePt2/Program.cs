188b0c9692cde74e146051a75ef26156156f75fd
Polka17 <poli.andonova03@gmail.com>
2023-05-23 16:23:14 +0300

    Somewhat of an optimisation

--- FootballGamePt2/Program.cs
+++ FootballGamePt2/Program.cs
@@ -1,6 +1,32 @@
 ﻿using FootballGamePt2;
+/*
+    SOME INFO YOU CAN USE TO COPY AS PLAYER INPUT FOR IT TO BE FASTER!!!
 
-static FootballPlayer[] GeneratePlayers( int count)
+    striker/Miro/21/4/184
+    defender/Iliyan/18/7/178
+    defender/Ivan/23/12/191
+    midfield/Petko/34/15/169
+    striker/Dimitur/25/10/173
+    midfield/Preslav/21/1/182
+    goalkeeper/Kris/27/25/181
+    midfield/Alex/22/3/177
+    defender/Mihail/18/5/180
+    striker/Pavel/19/17/165
+    striker/Milen/31/6/170
+
+    defender/Moni/21/4/180
+    defender/Ela/20/7/172
+    defender/Bora/22/12/188
+    midfield/Iva/19/15/169
+    striker/Dimi/25/10/178
+    midfield/Polka/20/1/162
+    striker/Kristin/20/25/164
+    midfield/Alex/22/3/179
+    defender/Mika/18/5/172
+    goalkeeper/Aria/19/17/165
+    striker/Rali/21/6/159
+ */
+static FootballPlayer[] GeneratePlayers(int count)
 {
     FootballPlayer[] playersC = new FootballPlayer[count];
 
@@ -47,81 +73,9 @@ static FootballPlayer[] GeneratePlayers( int count)
     }
     return playersC;
 }
-//Console.WriteLine("Enter the number of players in the firts team:");
-//int count = int.Parse(Console.ReadLine());
-//FootballPlayer[] teamOnePlayers = new FootballPlayer[count];
-//teamOnePlayers = GeneratePlayers(teamOnePlayers);
 
-//for (int i = 0; i < teamOnePlayers.Length; i++)
-//{
-//    Console.WriteLine("Enter player's position/name/age/number/height:");
-//    string[] playersData = Console.ReadLine().Split("/");
-//    if (playersData[0] == "striker")
-//    {
-//        Striker striker = new Striker();
-//        striker.Name = playersData[1];
-//        striker.Age = int.Parse(playersData[2]);
-//        striker.Number = int.Parse(playersData[3]);
-//        striker.Height = double.Parse(playersData[4]);
-//        teamOnePlayers[i] = striker;
-//    }
-//    if (playersData[0] == "midfield")
-//    {
-//        Midfield midfield = new Midfield();
-//        midfield.Name = playersData[1];
-//        midfield.Age = int.Parse(playersData[2]);
-//        midfield.Number = int.Parse(playersData[3]);
-//        midfield.Height = double.Parse(playersData[4]);
-//        teamOnePlayers[i] = midfield;
-//    }
-//    if (playersData[0] == "defender")
-//    {
-//        Defender defender = new Defender();
-//        defender.Name = playersData[1];
-//        defender.Age = int.Parse(playersData[2]);
-//        defender.Number = int.Parse(playersData[3]);
-//        defender.Height = double.Parse(playersData[4]);
-//        teamOnePlayers[i] = defender;
-//    }
-//    if (playersData[0] == "goalkeeper")
-//    {
-//        GoalKeeper goalKeeper = new GoalKeeper();
-//        goalKeeper.Name = playersData[1];
-//        goalKeeper.Age = int.Parse(playersData[2]);
-//        goalKeeper.Number = int.Parse(playersData[3]);
-//        goalKeeper.Height = double.Parse(playersData[4]);
-//        teamOnePlayers[i] = goalKeeper;
-//    }
-//}
-//FootballPlayer[] teamOnePlayers =
-//{
-//        new Striker { Name = "Polka", Age = 20, Number = 7, Height = 159},
-//        new Striker { Name = "Lora", Age = 18, Number = 8, Height = 164},
-//        new Striker { Name = "Maria", Age = 29, Number = 15, Height = 151},
-//        new Striker { Name = "Alex", Age = 22, Number = 12, Height = 178},
-//        new Striker { Name = "Plamen", Age = 27, Number = 4, Height = 171},
-//        new Striker { Name = "Venislav", Age = 31, Number = 6, Height = 194},
-//        new Striker { Name = "Kiril", Age = 22, Number = 20, Height = 175},
-//        new Striker { Name = "Kalina", Age = 20, Number = 1, Height = 167},
-//        new Striker { Name = "Iva", Age = 18, Number = 14, Height = 165},
-//        new Striker { Name = "Emil", Age = 28, Number = 10, Height = 192},
-//        new Striker { Name = "Peter", Age = 25, Number = 3, Height = 184},
-//};
-//FootballPlayer[] teamTwoPlayers =
-//{
-//        new Striker { Name = "Ralica", Age = 29, Number = 8, Height = 159},
-//        new Striker { Name = "Elena", Age = 31, Number = 4, Height = 164},
-//        new Striker { Name = "Velislava", Age = 24, Number = 11, Height = 151},
-//        new Striker { Name = "Kris", Age = 19, Number = 13, Height = 178},
-//        new Striker { Name = "Pamela", Age = 18, Number = 1, Height = 171},
-//        new Striker { Name = "Lucas", Age = 25, Number = 7, Height = 194},
-//        new Striker { Name = "Radoslav", Age = 21, Number = 18, Height = 175},
-//        new Striker { Name = "Hristina", Age = 20, Number = 1, Height = 167},
-//        new Striker { Name = "Eva", Age = 20, Number = 2, Height = 165},
-//        new Striker { Name = "Lubo", Age = 36, Number = 6, Height = 192},
-//        new Striker { Name = "Hristo", Age = 40, Number = 14, Height = 184},
-//};
 
+Console.WriteLine("----Coaches----");
 Console.WriteLine("Enter the number of coaches:");
 int coachCount = int.Parse(Console.ReadLine());
 Coach[] coaches = new Coach[coachCount];
@@ -134,13 +88,7 @@ for (int i = 0; i < coaches.Length; i++)
     coach.Age = int.Parse(Console.ReadLine());
     coaches[i] = coach;
 }
-//Coach[] coach =
-//{
-//    new Coach{ Name = "Stefan", Age = 50},
-//    new Coach{ Name = "Elica", Age = 38},
-//    new Coach{ Name = "Miroslav", Age = 42},
-//    new Coach{ Name = "Zdavko", Age = 40}
-//};
+
 
 Console.WriteLine("----Referees----");
 Referee[] referees = new Referee[3];
@@ -153,49 +101,42 @@ for (int i = 0; i < referees.Length; i++)
     referee.Age = int.Parse(Console.ReadLine());
     referees[i] = referee;
 }
-//Referee[] referees = 
-//{
-//    new Referee{Name ="Leo", Age = 38},
-//    new Referee{Name ="Yavor", Age = 31},
-//    new Referee{Name ="Anelia", Age = 29},
-//};
+
+
+Console.WriteLine("----Teams----");
 Console.WriteLine("Enter the number of teams:");
 int teamCount = int.Parse(Console.ReadLine());
 Team[] teams = new Team[teamCount];
-for (int i = 0; i < referees.Length-1; i++)
+for (int i = 0; i < referees.Length - 1; i++)
 {
     Team team = new Team();
-    Console.WriteLine("Enter team's coach number:");
+    Console.WriteLine("Enter team's coach number(array number):");
     int coachNum = int.Parse(Console.ReadLine());
     team.Coach = coaches[coachNum];
-    //Console.WriteLine("Enter team's players number:");
-    //int playersNum = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter the number of players in the firts team:");
+    Console.WriteLine($"Enter the number of players in the {i + 1} team:");
     int count = int.Parse(Console.ReadLine());
     team.Players = GeneratePlayers(count);
     teams[i] = team;
+    Console.WriteLine();
 }
 
-//Team[] teams = 
-//{
-//    new Team {Coach = coach[1], Players = teamOnePlayers },
-//    new Team {Coach = coach[3], Players = teamTwoPlayers },
-//};
-
+Console.WriteLine("----Game----");
 Game game = new Game();
-Console.WriteLine("Enter the first team's number:");
+Console.WriteLine("Enter the first team's number(array number):");
 int teamNum1 = int.Parse(Console.ReadLine());
 game.TeamOne = teams[teamNum1];
-Console.WriteLine("Enter the second team's number:");
+Console.WriteLine("Enter the second team's number(array number):");
 int teamNum2 = int.Parse(Console.ReadLine());
 game.TeamTwo = teams[teamNum2];
 game.Referees = referees;
 
+
+Console.WriteLine("----Goals----");
 Console.WriteLine("Enter the number of goals you want:");
 int goalsCount = int.Parse(Console.ReadLine());
 for (int i = 0; i < goalsCount; i++)
 {
-    Console.WriteLine("Enter the team number that scored:");
+    Console.WriteLine("Enter the team number(array number) that scored:");
     int teamNum = int.Parse(Console.ReadLine());
     if (teamNum == 1)
     {
@@ -214,9 +155,9 @@ for (int i = 0; i < goalsCount; i++)
         game.AddGoal(game.TeamTwo.Players[playerNum], minute);
     }
 }
-//game.AddGoal(game.TeamOne.Players[2], 45);
-//game.AddGoal(game.TeamOne.Players[7], 70);
-//game.AddGoal(game.TeamTwo.Players[1], 78); 
+Console.WriteLine();
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("Game Info");
 
 game.ChangeResult();
 game.GameResult();
